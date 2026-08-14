d6d997c8c08777f7550a625d0b8903480ab73edb
NandaniTripathi <151769122+NandaniTripathi@users.noreply.github.com>
2026-08-14 22:50:11 +0530

    Update llm_helper.py

--- llm_helper.py
+++ llm_helper.py
@@ -3,13 +3,18 @@ import os
 from dotenv import load_dotenv
 
 load_dotenv()
-llm = ChatGroq(groq_api_key=os.getenv("GROQ_API_KEY"), model_name="llama-3.2-90b-text-preview")
 
+llm = ChatGroq(
+    groq_api_key=os.getenv("GROQ_API_KEY"),
+    model_name="llama-3.3-70b-versatile"
+)
 
 if __name__ == "__main__":
-    response = llm.invoke("Two most important ingradient in samosa are ")
-    print(response.content)
+    response = llm.invoke(
+        "Two most important ingredients in samosa are"
+    )
 
+    print(response.content)
 
 
 
